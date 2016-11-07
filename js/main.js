d927d4a2dc29fc484dea10fcaa587c3a49e81cc8
Victor Widell <victor@topmost.se>
2016-11-07 17:43:48 +0100

    A clicky button.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+var ClickyView = Backbone.View.extend({
+
+	tagName: 'button',
+
+	events: {
+		'click': 'updateView'
+	},
+
+	updateView: function () {
+
+		this.$el.text('Num clicks: 1');
+	}
+});
+
+
+
+var body = document.getElementsByTagName('body')[0];
+
+var clickyView = new ClickyView();
+body.appendChild(clickyView.el);
