b75838e91315ee6d8cea00626a67af02e11b54a4
Victor Widell <victor@topmost.se>
2016-11-07 17:51:42 +0100

    A counting model.

--- js/main.js
+++ js/main.js
@@ -5,18 +5,49 @@ var ClickyView = Backbone.View.extend({
 	tagName: 'button',
 
 	events: {
-		'click': 'updateView'
+		'click': 'onClick'
+	},
+
+
+	onClick: function () {
+
+		this.model.click();
+	},
+
+	initialize: function (options) {
+
+		this.listenTo(this.model, 'change', this.updateView, this);
+		this.updateView();
 	},
 
 	updateView: function () {
 
-		this.$el.text('Num clicks: 1');
+		this.$el.text('Num clicks: '+this.model.get('count'));
 	}
 });
 
 
+var Clicky = Backbone.Model.extend({
+
+	defaults: {
+		count: 0
+	},
+
+	click: function () {
+
+		this.set('count', this.get('count') + 1);
+	}
+});
+
 
 var body = document.getElementsByTagName('body')[0];
 
-var clickyView = new ClickyView();
+var clicky = new Clicky();
+var clickyView = new ClickyView({
+	model: clicky
+});
 body.appendChild(clickyView.el);
+
+setInterval(function () {
+	clicky.click();
+}, 2000);
